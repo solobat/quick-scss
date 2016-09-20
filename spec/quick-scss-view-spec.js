79dcc6ad709b881e504ee85e0367f94fd8e8de61
杨建川 <yangjianchuan@letv.com>
2016-09-21 00:48:37 +0800

    init

---
+++ spec/quick-scss-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import QuickScssView from '../lib/quick-scss-view';
+
+describe('QuickScssView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
